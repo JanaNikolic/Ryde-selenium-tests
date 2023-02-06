d16cf4dbcee39a3aee7e68e431368c58cba72ef6
Bogdan <bogdanjanosevic@gmail.com>
2023-02-06 14:25:18 +0100

    Selenium tests for login

---
+++ Ryde_Tim17/src/test/java/org/example/pages/PassengerPage.java
@@ -0,0 +1,33 @@
+package org.example.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class PassengerPage {
+    WebDriver driver;
+
+    @FindBy(css = "h1")
+    private WebElement getRydeText;
+
+    @FindBy(id = "logoutButton")
+    WebElement logoutButton;
+
+    public PassengerPage(WebDriver driver){
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public boolean passengerIsOpened() {
+        return (new WebDriverWait(driver, Duration.ofSeconds(10)))
+                .until(ExpectedConditions.textToBePresentInElement(getRydeText, "Get A Ryde"));
+    }
+    public void logoutPassenger(){
+        logoutButton.click();
+    }
+}
